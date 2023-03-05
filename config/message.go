a56c6bae1eb90e2e274e46ba147be2dec7e7a29d
robinmin <luonbin@hotmail.com>
2023-03-05 18:22:03 +0800

    Add i18n support

--- config/message.go
+++ config/message.go
@@ -1,7 +1,8 @@
 package config
 
+var CurrentLanguage string
+
 var i18n_str_table = map[string]string{
-	// "greeting_msg": "您好，我是您的私人助理x-ally, 请问有什么可以帮您？帮助请输入@help、退出请输入@quit",
 	"greeting_msg": `# %s (%s)
 
 您好，我是您的私人助理%s, 请问有什么可以帮您？  [ %s ]
@@ -20,16 +21,92 @@ var i18n_str_table = map[string]string{
 	"tips_suggestion_quit":      "退出本程序",
 	"tips_suggestion_reset":     "重置机器人角色为：",
 	"tips_suggestion_cmd":       "执行本地命令，并将结果回显",
-	"tips_suggestion_ask":       "问chatGPT",
-	"tips_suggestion_translate": "用deepl翻译或查字典",
+	"tips_suggestion_ask":       "问ChatGPT",
+	"tips_suggestion_translate": "用DeepL翻译或查字典",
 	"tips_changed_role":         "已为您切换为%s%s, %s",
+}
+
+var i18n_str_table_en = map[string]string{
+	"greeting_msg": `# %s (%s)
+
+Hello, I am your personal assistant%s, how can I help you?  [ %s ]
+`,
+	"byebye_msg": "Okay, see you later!👋🏻",
+
+	"sys_invalid_cmd":    "Invalid internal command or not supported by current version",
+	"sys_not_enough_cmd": "nvalid command to execute (length greater than 2)",
+
+	"error_no_chatgpt_key": "Missing environment variable OPENAI_API_KEY, please specify or apply at [openai.com](https://platform.openai.com/account/api-keys)",
+	"error_no_deepl_key":   "Missing environment variable DEEPL_API_KEY, please specify or apply at [deepl.com](https://www.deepl.com/pro-api?cta=header-pro-api/)",
+
+	"error_failed_exec":  "Failed to execute command: ",
+	"error_invalid_role": "An invalid role (%s) was specified and has been reset to the default role: ",
+
+	"tips_suggestion_quit":      "Exit",
+	"tips_suggestion_reset":     "Reset the robot role to: ",
+	"tips_suggestion_cmd":       "Execute local commands and display the results",
+	"tips_suggestion_ask":       "Ask ChatGPT",
+	"tips_suggestion_translate": "Use DeepL to translate or look up the dictionary",
+	"tips_changed_role":         "Switched to %s%s, %s",
+}
+
+var i18n_str_table_jp = map[string]string{
+	"greeting_msg": `# %s (%s)
+
+こんにちは、私はあなたのパーソナルアシスタント%sです、あなたのために何ができますか？  [ %s ]
+`,
+	"byebye_msg": "じゃあ、またね！👋🏻",
+
+	"sys_invalid_cmd":    "不正な内部コマンドまたは現在のバージョンでサポートされていない",
+	"sys_not_enough_cmd": "実行する有効なコマンドがない（長さが2以上）",
+
+	"error_no_chatgpt_key": "環境変数OPENAI_API_KEYが不足しています。指定するか、[openai.com](https://platform.openai.com/account/api-keys)からリクエストしてください。",
+	"error_no_deepl_key":   "環境変数DEEPL_API_KEYがありません。[deepl.com](https://www.deepl.com/pro-api?cta=header-pro-api/)で指定またはリクエストしてください。",
+
+	"error_failed_exec":  "実行失敗：",
+	"error_invalid_role": "無効なロール (%s) が指定されたので、デフォルトのロールにリセットされました：",
+
+	"tips_suggestion_quit":      "終了する",
+	"tips_suggestion_reset":     "ロボットの役割をリセットして：",
+	"tips_suggestion_cmd":       "ローカルコマンドを実行し、その結果を表示する",
+	"tips_suggestion_ask":       "ChatGPTに問い合わせて",
+	"tips_suggestion_translate": "DeepLで翻訳する、または辞書を調べて",
+	"tips_changed_role":         "スウィッチフォーユー %s%s, %s",
 }
 
 // //////////////////////////////////////////////////////////////////////////////
 func Text(str_key string) string {
-	if str_val, ok := i18n_str_table[str_key]; ok {
-		return str_val
+	msg := ""
+	switch CurrentLanguage {
+	case "EN", "en_US.UTF-8", "C":
+		if str_val, ok := i18n_str_table_en[str_key]; ok {
+			msg = str_val
+		} else {
+			msg = str_key
+		}
+	case "JP", "ja_JP.UTF-8":
+		if str_val, ok := i18n_str_table_jp[str_key]; ok {
+			msg = str_val
+		} else {
+			msg = str_key
+		}
+	case "CN", "zh_CN.UTF-8":
+		fallthrough
+	default:
+		if str_val, ok := i18n_str_table[str_key]; ok {
+			msg = str_val
+		} else {
+			msg = str_key
+		}
+	}
+	return msg
+}
+
+func SelectLang(lang string) {
+	if lang == "JP" || lang == "EN" {
+		CurrentLanguage = lang
 	} else {
-		return str_key
+		CurrentLanguage = LanguagePreference
+
 	}
 }
