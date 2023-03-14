bfec045adfb68a2e472b7279d7ce9d1ab9013d88
robinmin <luonbin@hotmail.com>
2023-03-15 00:45:12 +0800

    add LoadServerConfig

--- config/server.go
+++ config/server.go
@@ -1,23 +1,114 @@
 package config
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+
+	"gopkg.in/yaml.v3"
+)
+
 // Real constants and can not be changed
 const ServerName = "X-Ally-Server"
 const ServerVersion = "0.0.5"
 
-type ServerConfig struct {
-	DSN        string
-	ServerPort string
+type ProxyRoute struct {
+	Name    string `yaml:"name"`
+	Context string `yaml:"context"`
+	Target  string `yaml:"target"`
+}
 
-	TokenApiSecret string
-	TokenLifespan  uint32
+type ServerConfigItems struct {
+	DSN              string       `yaml:"DSN"`
+	OpenaiApiKey     string       `yaml:"openai_api_key"`
+	OpenaiOrgID      string       `yaml:"openai_org_id"`
+	APITokenSecret   string       `yaml:"api_token_secret"`
+	APITokenLifespan uint32       `yaml:"api_token_lifespan"`
+	ListenAddr       string       `yaml:"listen_addr"`
+	Routes           []ProxyRoute `yaml:"routes,omitempty"`
+}
+
+type ServerConfig struct {
+	Server *ServerConfigItems `yaml:"server"`
+}
+
+func (cfg *ServerConfig) DumpIntoYAML(cfg_file string) (string, error) {
+	yaml_data, err := yaml.Marshal(cfg)
+	if err != nil {
+		return "", err
+	}
+
+	if err = os.WriteFile(cfg_file, yaml_data, 0644); err != nil {
+		return "", err
+	}
+	return string(yaml_data), nil
+}
+
+func (cfg *ServerConfig) LoadFromYAML(cfg_file string) error {
+	var data []byte
+	var err error
+
+	if data, err = os.ReadFile(cfg_file); err != nil {
+		return err
+	}
+	if err = yaml.Unmarshal(data, cfg); err != nil {
+		return err
+	}
+	return nil
 }
 
 var SvrConfig *ServerConfig
 
 func LoadServerConfig(config_file string, verbose bool) (*ServerConfig, error) {
-	SvrConfig = &ServerConfig{
-		DSN:        "robin:dragon2001@tcp(127.0.0.1:3306)/xally?charset=utf8&parseTime=True&loc=Local",
-		ServerPort: ":8080",
+	SvrConfig = &ServerConfig{}
+
+	var temp_file string
+	var default_config_file string
+	var err error
+	if config_file == "" {
+		var dir_home string
+		var err error
+		if dir_home, err = FindHomeDir(verbose); err != nil {
+			return nil, errors.New("Failed to find home directory")
+		}
+		default_config_file = path.Join(dir_home, "xally_server.yaml")
+		temp_file = default_config_file
+	} else {
+		temp_file = config_file
+	}
+
+	// Create config structure
+	skip_reload := false
+	if _, err = os.Stat(temp_file); os.IsNotExist(err) {
+		// generate default config file
+		if _, err = SvrConfig.DumpIntoYAML(temp_file); err != nil {
+			if verbose {
+				fmt.Println("Failed to write YAML data into :", temp_file)
+			}
+			temp_file = default_config_file
+		} else {
+			skip_reload = true
+		}
+	}
+
+	// // Open config file
+	if !skip_reload {
+		if verbose {
+			fmt.Println("Loading config file from ", temp_file)
+		}
+
+		if err = SvrConfig.LoadFromYAML(temp_file); err != nil {
+			if verbose {
+				fmt.Println("Failed to load configuration from config file : ", temp_file)
+				fmt.Println(err)
+			}
+			return SvrConfig, err
+		}
+		// update key from env var in case of blank
+		if SvrConfig.Server.OpenaiApiKey == "" {
+			SvrConfig.Server.OpenaiApiKey = os.Getenv("OPENAI_API_KEY")
+		}
 	}
 
 	return SvrConfig, nil
