e20bf549e4ed04a3bb5f9acb13b1edd3a8fde00a
robinmin <luonbin@hotmail.com>
2023-03-20 21:06:21 +0800

    upgrade version number into 0.1.0

--- config/server.go
+++ config/server.go
@@ -11,7 +11,7 @@ import (
 
 // Real constants and can not be changed
 const ServerName = "X-Ally-Server"
-const ServerVersion = "0.0.5"
+const ServerVersion = "0.1.0"
 
 type ProxyRoute struct {
 	Name    string `yaml:"name"`
